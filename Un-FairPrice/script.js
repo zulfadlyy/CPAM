118314031486302435cca0b2c388d6426c065eb0
zoofatlee <zulfadlyyusoff@gmail.com>
2024-02-16 01:53:16 +0800

    add stuff

---
+++ Un-FairPrice/script.js
@@ -0,0 +1,15 @@
+var numberField = document.getElementById('numberField');
+ 
+    function increment() {
+      var currentValue = parseInt(numberField.value);
+      if (currentValue < 5) {
+        numberField.value = currentValue + 1;
+      }
+    }
+ 
+    function decrement() {
+      var currentValue = parseInt(numberField.value);
+      if (currentValue > 0) {
+        numberField.value = currentValue - 1;
+      }
+    }
